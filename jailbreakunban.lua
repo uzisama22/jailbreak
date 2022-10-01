3870fa8e978da99b8f0b558f40c3b51395332f3d
uzisama22 <101911122+uzisama22@users.noreply.github.com>
2022-10-01 16:24:55 -0700

    Add files via upload

---
+++ jailbreakunban.lua
@@ -0,0 +1,59 @@
+---Raw Script---
+---Unban Script leaked
+--Unban timer has to be 20
+
+
+Player.Id.join = "load"
+load.player.mesh.Id = "Id/UserId"
+Refresh.Player.Mesh = true
+Set.BanTimer = "20"
+Refresh.Player.20.seconds = true
+unjail.Player.mesh = false
+jail.player.mesh = true
+load.jailtimer = false
+wait(0)
+print("jailTimer Here")
+wait(1)
+print("timer : 100")
+wait(1)
+print("timer : 99")
+wait(1)
+print("timer : 98")
+wait(1)
+print("timer : 97")
+wait(1)
+print("timer : 96")
+wait(1)
+print("timer : 95")
+wait(1)
+print("timer : 94")
+wait(1)
+print("timer : 93")
+wait(1)
+print("timer : 92")
+wait(1)
+print("timer : 91")
+wait(1)
+print("timer : 90")
+wait(1)
+print("timer : 100")
+wait(1)
+print("timer : 99")
+wait(1)
+print("timer : 98")
+wait(1)
+print("timer : 97")
+wait(1)
+print("timer : 96")
+wait(1)
+print("timer : 95")
+wait(1)
+print("timer : 94")
+wait(1)
+print("timer : 93")
+wait(1)
+print("timer : 92")
+wait(1)
+print("timer : 91")
+wait(1)
+print("timer : 90")
---
+++ jailbreakunban2.lua
@@ -0,0 +1,20 @@
+---works for jailbreak and other games instead of da hood
+load.char.jailtimer = true
+load.print.time = true
+set.time = 10
+wait(2)
+print("loading...")
+wait(1)
+print("loaded!")
+wait(1)
+print("timer : 10")
+wait(1)
+print("timer : 9")
+wait(1)
+print("timer : 8")
+wait(1)
+print("Error!")
+wait(1)
+print("Timer : 0")
+wait(1)
+print("unbanned!")
